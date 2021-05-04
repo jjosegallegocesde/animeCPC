85fc1a95d74ef907c079b3dc6dc4de3ea59e5df4
Juan <juan.gallegomesa@gmail.com>
2021-05-04 16:42:58 -0500

    se crea lista dinamica

--- scripts/conexion.js
+++ scripts/conexion.js
@@ -1,6 +1,15 @@
-let busqueda="anime";
+let busqueda="goku";
 let url=`http://api.giphy.com/v1/gifs/search?api_key=fEAR9TrqgjatwcOtKAPuEkthH7dg1Z9k&q=${busqueda}`;
 
+
+let botonBusqueda=document.getElementById("botonBusqueda");
+let parametroBusqueda=document.getElementById("busqueda");
+
+botonBusqueda.addEventListener("click",buscarDatos);
+
+function buscarDatos(){
+    console.log("hola estoy buscando....");
+}
 
 fetch(url)
     .then(respuesta=>respuesta.json())
@@ -9,35 +18,18 @@ fetch(url)
 
 function organizarDatos(datos){
 
-    console.log(datos);
-    console.log(datos.data);
-    console.log(datos.data[0]);
-    console.log(datos.data[0].images);
-    console.log(datos.data[0].images.downsized_medium);
-    console.log(datos.data[0].images.downsized_medium.url);
+    let contenedorPadre=document.getElementById("contenedor");
+    let arregloDatos=datos.data;
 
-    let imagen1=document.getElementById("img1");
-    let imagen2=document.getElementById("img2");
-    let imagen3=document.getElementById("img3");
-    let imagen4=document.getElementById("img4");
-    let imagen5=document.getElementById("img5");
-    let imagen6=document.getElementById("img6");
-    let imagen7=document.getElementById("img7");
-    let imagen8=document.getElementById("img8");
-    let imagen9=document.getElementById("img9");
-    let imagen10=document.getElementById("img10");
+    arregloDatos.map(function(dato){
+        
+        let imagen=document.createElement("img");
 
-    imagen1.src=datos.data[0].images.downsized_medium.url;
-    imagen2.src=datos.data[1].images.downsized_medium.url;
-    imagen3.src=datos.data[2].images.downsized_medium.url;
-    imagen4.src=datos.data[3].images.downsized_medium.url;
-    imagen5.src=datos.data[4].images.downsized_medium.url;
-    imagen6.src=datos.data[5].images.downsized_medium.url;
-    imagen7.src=datos.data[10].images.downsized_medium.url;
-    imagen8.src=datos.data[7].images.downsized_medium.url;
-    imagen9.src=datos.data[8].images.downsized_medium.url;
-    imagen10.src=datos.data[9].images.downsized_medium.url;
-    
+        imagen.src=dato.images.downsized_medium.url;
+        contenedorPadre.appendChild(imagen);
+
+    });
+
 
 
 
