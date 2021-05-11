1fe0b4f54f8696b8b0793a2ae70371895bc2fb99
Juan <juan.gallegomesa@gmail.com>
2021-05-11 16:26:25 -0500

    se crea formulario de registro 2

--- scripts/controladorRegistro.js
+++ scripts/controladorRegistro.js
@@ -1,1 +1,23 @@
+//Se referencias las etiquetas a controlar
 let boton=document.getElementById("botonEnvio");
+let nombre=document.getElementById("nombreUsuario");
+let correo=document.getElementById("correoUsuario");
+let password1=document.getElementById("passwordUsuario");
+
+
+//Recibir el evento de clic en mi boton
+boton.addEventListener("click",recibirDatosFormulario);
+
+//Se crea función para recibir información
+function recibirDatosFormulario(){
+
+    let nombreValor=nombre.value;
+    let correoValor=correo.value;
+    let password1Valor=password1.value;
+   
+}
+
+
+
+
+
